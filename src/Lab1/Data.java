383435becbebf6eb6798f5f15aa716b803b78808
artisoul0 <yur4ikff12@gmail.com>
2022-11-21 19:44:15 +0200

    some changes

--- src/Lab1/Data.java
+++ src/Lab1/Data.java
@@ -29,114 +29,76 @@ public class Data {
     }
 
     //Create vectors and matrices
-    public static int[][] MV;
-    public static int[][] MC;
     public static int[][] MM;
+    public static int[][] MX;
+    public static int[][] MT;
 
-    public static int[] R;
-    public static int[] B;
-    public static int[] X;
-    public static int e;
+    public static int[] A;
     public static int[] Z;
+    public static int[] B;
+    public static int p;
 
 
     //Getters for all vectors and matrices
-    public static int[][] getMV() {
-        return MV;
-    }
-
-    public static int[][] getMC() {
-        return MC;
-    }
-
     public static int[][] getMM() {
         return MM;
     }
 
-    public static int[] getR() {
-        return R;
+    public static int[][] getMX() {
+        return MX;
+    }
+
+    public static int[][] getMT() {
+        return MT;
+    }
+
+    public static int[] getA() {
+        return A;
     }
 
     public static int[] getB() {
         return B;
     }
 
-    public static int[] getX() {
-        return X;
-    }
-
-    public static int gete() {
-        return e;
-    }
-
     public static int[] getZ() {
         return Z;
     }
 
+
+    public static int getp() {
+        return p;
+    }
+
     //Setting the vectors and matrices
-
-    public static void adjustMV(int[][] MV) {
-        Data.MV = MV;
-    }
-
-    public static void adjustR(int[] R) {
-        Data.R = R;
-    }
-
-    public static void adjustMC(int[][] MC) {
-        Data.MC = MC;
-    }
 
     public static void adjustMM(int[][] MM) {
         Data.MM = MM;
+    }
+
+    public static void adjustA(int[] A) {
+        Data.A = A;
+    }
+
+    public static void adjustMX(int[][] MX) {
+        Data.MX = MX;
+    }
+
+    public static void adjustMT(int[][] MT) {
+        Data.MT = MT;
     }
 
     public static void adjustB(int[] B) {
         Data.B = B;
     }
 
-    public static void adjustX(int[] X) {
-        Data.X = X;
-    }
-
-    public static void adjuste(int e) {
-        Data.e = e;
-    }
-
     public static void adjustZ(int[] Z) {
         Data.Z = Z;
     }
 
-
-    //The method to get a max element in vector Z
-    public static int getMaxInQuarterVector(int[] Vector) {
-        int scalarMaxValue = 0;
-        for (int j : Vector) {
-            if (scalarMaxValue < j) {
-                scalarMaxValue = j;
-            }
-        }
-        return scalarMaxValue;
+    public static void adjustp(int p) {
+        Data.p = p;
     }
 
-    //The method to compare in calculation: d = max(d,di)
-    public static boolean firstPair = true;
-
-    public static int maxScalarD(AtomicInteger d, int di) {
-        if (firstPair) {
-            firstPair = false;
-            return di;
-        }
-        return Math.max(d.get(), di);
-    }
-    //Calculation Rн = d*(B * MVн) + е*Х*(MM * MCн)
-
-    //Set the part of the vector R
-
-    public static void setResultPartOfVectorR(int d, int[] B, int[][] MV, int e, int[] X, int[][] MM, int[][] MC, int start, int end) {
-        int[] finalVector = sumVector(multiplySubVectorByConstant(d, multiplyVectorBySubMatrix(B, MV, start, end), start, end), multiplyVectorBySubMatrix(multiplyScalarAndVector(e, X), multiplyMatrixAndSubMatrix(MM, MC, start, end), start, end), start, end);
-        Write.writeToResult(finalVector, start, end);
-    }
 
     //Method to multiply All Vector and SubMatrix
 
@@ -153,27 +115,13 @@ public class Data {
         return K;
     }
 
+    //Method to multiply constant by SubVector
     private static int[] multiplySubVectorByConstant(int a, int[] C, int
             start, int end) {
         for (int i = start; i < end; i++) {
             C[i] *= a;
         }
         return C;
-    }
-
-    public static int[] multiplyScalarAndVector(int scalar, int[] Vector) {
-        int[] result = new int[Data.N];
-        for (int i = 0; i < Data.N; i++) {
-            result[i] = scalar * Vector[i];
-        }
-        return result;
-    }
-
-    private static int[] sumVector(int[] X, int[] Y, int start, int end) {
-        for (int i = start; i < end; i++) {
-            X[i] = X[i] + Y[i];
-        }
-        return X;
     }
 
 
